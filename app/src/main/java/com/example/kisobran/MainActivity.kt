a2675367510197fccbb0e86d1169f982b0080888
NavekZadji <brunno.hrastic@gmail.com>
2022-12-27 14:12:22 +0100

    Location

--- app/src/main/java/com/example/kisobran/MainActivity.kt
+++ app/src/main/java/com/example/kisobran/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.location.FusedLocationProviderClient
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -13,20 +14,26 @@ import org.koin.core.component.KoinComponent
 class MainActivity : AppCompatActivity(), KoinComponent {
 
     private val viewModel by viewModel<KisobranViewModel>()
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var ZemljopisnaSirina : Double = 0.0
+    private var ZemljopisnaDuzina : Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lifecycleScope.launch {
-            val locationTracker: LocationTracker
-            locationTracker.getCurrentLocation()?.let { location ->
-                when(val result = viewModel.getKisobran()){
-                    //u slucaju uspjeha
-                }
-            }
 
+        // napraviti check permission ovdje umjesto u DefaultLocationTracker, mozda tako mogu izbjeci dependancy injection
+
+    fun getLastKnownLocation(){
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            location ->
+            if(location != null){
+                ZemljopisnaSirina = location.latitude
+                ZemljopisnaDuzina = location.longitude
+            }
         }
+    }
 
     }
 
