1d4c655afbc50b1376c5d0ae886f0d29fb7a0b0b
NavekZadji <brunno.hrastic@gmail.com>
2022-12-28 18:40:33 +0100

    Location

--- app/src/main/java/com/example/kisobran/MainActivity.kt
+++ app/src/main/java/com/example/kisobran/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.kisobran.databinding.ActivityMainBinding
+import com.google.android.gms.common.internal.FallbackServiceBroker
 import com.google.android.gms.location.FusedLocationProviderClient
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.launch
@@ -54,7 +55,7 @@ class MainActivity : AppCompatActivity(), KoinComponent {
                 KisobranApplication.appContext, android.Manifest.permission.ACCESS_COARSE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
 
-            if (!hasAccessCoarseLocationPermission && !hasAccessFineLocationPermission) {
+            if (hasAccessCoarseLocationPermission && hasAccessFineLocationPermission) {
                 fusedLocationClient?.lastLocation?.addOnSuccessListener { location ->
                     if (location != null) {
                         zemljopisnaSirina = location.latitude
@@ -85,38 +86,40 @@ class MainActivity : AppCompatActivity(), KoinComponent {
             val timeNiz = ulazniPodatci.body()?.hourly?.time
             var prviKisovitiIndex = 0
 
-            while(prviKisovitiIndex < weathercodeNiz!!.size){
-                if(weathercodeNiz[prviKisovitiIndex] == 51 || weathercodeNiz[prviKisovitiIndex] == 53 || weathercodeNiz[prviKisovitiIndex] == 55 || weathercodeNiz[prviKisovitiIndex] == 56 || weathercodeNiz[prviKisovitiIndex] == 57 || weathercodeNiz[prviKisovitiIndex] == 61 || weathercodeNiz[prviKisovitiIndex] == 63 || weathercodeNiz[prviKisovitiIndex] == 65 || weathercodeNiz[prviKisovitiIndex] == 66 || weathercodeNiz[prviKisovitiIndex] == 67 || weathercodeNiz[prviKisovitiIndex] == 80 || weathercodeNiz[prviKisovitiIndex] == 81 || weathercodeNiz[prviKisovitiIndex] == 82 || weathercodeNiz[prviKisovitiIndex] == 95 || weathercodeNiz[prviKisovitiIndex] == 96 || weathercodeNiz[prviKisovitiIndex] == 99){
+            while (prviKisovitiIndex < weathercodeNiz!!.size) {
+                if (weathercodeNiz[prviKisovitiIndex] == 51 || weathercodeNiz[prviKisovitiIndex] == 53 || weathercodeNiz[prviKisovitiIndex] == 55 || weathercodeNiz[prviKisovitiIndex] == 56 || weathercodeNiz[prviKisovitiIndex] == 57 || weathercodeNiz[prviKisovitiIndex] == 61 || weathercodeNiz[prviKisovitiIndex] == 63 || weathercodeNiz[prviKisovitiIndex] == 65 || weathercodeNiz[prviKisovitiIndex] == 66 || weathercodeNiz[prviKisovitiIndex] == 67 || weathercodeNiz[prviKisovitiIndex] == 80 || weathercodeNiz[prviKisovitiIndex] == 81 || weathercodeNiz[prviKisovitiIndex] == 82 || weathercodeNiz[prviKisovitiIndex] == 95 || weathercodeNiz[prviKisovitiIndex] == 96 || weathercodeNiz[prviKisovitiIndex] == 99) {
                     break
                 }
                 prviKisovitiIndex++
             }
 
-            val prvoKisovitoVrijeme = timeNiz!![prviKisovitiIndex]
+            var susa = 0
+
+            if (prviKisovitiIndex == weathercodeNiz.size) {
+                susa = 1
+            }
+
+            if (susa == 0) {
+                val prvoKisovitoVrijeme = timeNiz!![prviKisovitiIndex]
 
 
-            val vrijemeKise = LocalDateTime.parse(prvoKisovitoVrijeme, formatter)
+                val vrijemeKise = LocalDateTime.parse(prvoKisovitoVrijeme, formatter)
 
-            val daniDoKise: Long = ChronoUnit.DAYS.between(vrijemeSad2, vrijemeKise)
-            val satiDoKise: Long = ChronoUnit.HOURS.between(vrijemeSad2, vrijemeKise) % 24
+                val daniDoKise: Long = ChronoUnit.DAYS.between(vrijemeSad2, vrijemeKise)
+                val satiDoKise: Long = ChronoUnit.HOURS.between(vrijemeSad2, vrijemeKise) % 24
 
-            val porukaIspisa : String = "Prva kisa ce biti ${vrijemeKise.dayOfMonth}.${vrijemeKise.monthValue}.${vrijemeKise.year} u " +
-                    "${vrijemeKise.hour} sati , sto je za $daniDoKise dana i $satiDoKise sati"
+                val porukaIspisa: String =
+                    "Prva kisa ce biti ${vrijemeKise.dayOfMonth}.${vrijemeKise.monthValue}.${vrijemeKise.year} u " +
+                            "${vrijemeKise.hour} sati , sto je za $daniDoKise dana i $satiDoKise sati"
 
-            porukaFinal.text = porukaIspisa
+                porukaFinal.text = porukaIspisa
+            }
 
-            // probni ispisi, izbrisati poslije!
+            if (susa == 1) {
+                val porukaIspisa: String = "Nece biti kise u dogledno vrijeme."
+                porukaFinal.text = porukaIspisa
+            }
 
-            Log.v("ovdje", ulazniPrimjer!!)
-            Log.v("sad sati", vrijemeSad2.toString())
-            Log.v("sati do kise", satiDoKise.toString())
-            Log.v("dani do kise", daniDoKise.toString())
-            Log.v("prvi kisoviti sat", prvoKisovitoVrijeme)
-            Log.v("konacna poruka", porukaIspisa)
         }
-
-
     }
-
-
 }
